ffdbff4e35d450d49826cc8ff73b17d0d3a01b6b
ridwans15 <ridwan.sophian.150304@gmail.com>
2021-09-13 08:55:44 +0700

    commit mentoring pertemuan 9/10

---
+++ main.js
@@ -0,0 +1,13 @@
+// console.log('Hello World')
+
+const myName = "Ridwan Sophian"
+const btn = document.getElementById('my-name')
+
+// const onClick = () => {
+//     alert(myName)
+// }
+
+btn.addEventListener('click', function(){
+    alert(myName)
+})
+
